6af9d00595a9d05463e1aee22b7cc472c3cf2ff5
TRVRNB <travis.renberg@gmail.com>
2025-10-09 10:52:54 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// stores a list of students, their ID, and their GPA
+// has ADD, PRINT, DELETE, and HELP functions
+#include <iostream>
+#include <vector> // for vector data type
+using namespace std;
+
+struct Student{
+  // struct used for storing student values
+  char name1[81] = "Anita"; // first name
+  char name2[81] = "Jabłońskí"; // last name
+  int id = 123456; // ID
+  float gpa = 3.0; // GPA
+};
+  
+int main(){
+  return 0;
+}
